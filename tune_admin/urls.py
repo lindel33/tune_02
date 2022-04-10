b3af635a97465c48b37dd425a4836b06832ea1ab
Сергей Островский <81719949+lindel33@users.noreply.github.com>
2022-04-10 07:10:04 +0300

    Update urls.py

--- tune_admin/urls.py
+++ tune_admin/urls.py
@@ -2,12 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from .views import bot
+# from .views import bot
 from django.views.decorators.cache import cache_page
 
 
 urlpatterns = [
     path('/home', admin.site.urls),
-    path('/b', cache_page(100)(bot))
+#     path('/b', cache_page(100)(bot))
 
 ]
