7bc76de87ed5eb720b21114d55a2ee7e9a6dc4d0
Сергей Островский <81719949+lindel33@users.noreply.github.com>
2022-04-10 07:08:15 +0300

    Update admin.py

--- price/admin.py
+++ price/admin.py
@@ -1,37 +1,37 @@
-from django.contrib import admin
-from .models import Global, Iphone, Markup, Ipad, MacBook1, Watch, SpecialCharacter
-from cost_models.models import DetailModel
+# from django.contrib import admin
+# from .models import Global, Iphone, Markup, Ipad, MacBook1, Watch, SpecialCharacter
+# from cost_models.models import DetailModel
 
 
-@admin.register(Watch)
-class WatchAdmin(admin.ModelAdmin):
-    pass
+# @admin.register(Watch)
+# class WatchAdmin(admin.ModelAdmin):
+#     pass
 
 
-@admin.register(MacBook1)
-class MacBookAdmin(admin.ModelAdmin):
-    pass
+# @admin.register(MacBook1)
+# class MacBookAdmin(admin.ModelAdmin):
+#     pass
 
 
-@admin.register(Ipad)
-class IpadAdmin(admin.ModelAdmin):
-    pass
+# @admin.register(Ipad)
+# class IpadAdmin(admin.ModelAdmin):
+#     pass
 
-@admin.register(Global)
-class GlobalAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(Iphone)
-class IphoneAdmin(admin.ModelAdmin):
-    pass
+# @admin.register(Global)
+# class GlobalAdmin(admin.ModelAdmin):
+#     pass
 
 
-@admin.register(Markup)
-class MarkupAdmin(admin.ModelAdmin):
-    pass
+# @admin.register(Iphone)
+# class IphoneAdmin(admin.ModelAdmin):
+#     pass
 
 
-@admin.register(SpecialCharacter)
-class SpecialCharacterAdmin(admin.ModelAdmin):
-    list_display = ['provider_variant', 'new_variant', ]
+# @admin.register(Markup)
+# class MarkupAdmin(admin.ModelAdmin):
+#     pass
+
+
+# @admin.register(SpecialCharacter)
+# class SpecialCharacterAdmin(admin.ModelAdmin):
+#     list_display = ['provider_variant', 'new_variant', ]
