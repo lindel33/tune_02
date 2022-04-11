97b65f0d61fca0c9630bbceeb913200b7054b76a
Сергей Островский <81719949+lindel33@users.noreply.github.com>
2022-04-11 19:32:08 +0300

    Update urls.py

--- tune/urls.py
+++ tune/urls.py
@@ -6,7 +6,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('tune_admin.urls')),
-    path('api', include('bot_telegram.urls')),
-    path('some/', include('cost_models.urls')),
+#     path('api', include('bot_telegram.urls')),
+    path('csv_check/', include('cost_models.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
