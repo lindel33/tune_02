52d3d46d4e0e8e642cb05b3165b9dd2801ccfffd
Сергей Островский <81719949+lindel33@users.noreply.github.com>
2022-04-11 19:54:51 +0300

    Update urls.py

--- tune_admin/urls.py
+++ tune_admin/urls.py
@@ -7,7 +7,6 @@ from django.views.decorators.cache import cache_page
 
 
 urlpatterns = [
-    path('/', admin.site.urls),
-    path('api', cache_page(100)(bot))
+    path('/v1', cache_page(100)(bot))
 
 ]
